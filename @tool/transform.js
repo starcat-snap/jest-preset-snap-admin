bcd55fee81bb8a455661df392b60371bfac2cccb
wang <lyricsong47@gmail.com>
2023-12-20 15:24:33 +0800

    first commit

---
+++ @tool/transform.js
@@ -0,0 +1,13 @@
+const babelOptions = {
+  presets: ["@babel/preset-typescript"],
+  plugins: ["require-context-hook"],
+  env: {
+    test: {
+      presets: ["@babel/preset-env"],
+    },
+  },
+  ignore: [],
+  exclude: [],
+};
+
+module.exports = require("babel-jest").default.createTransformer(babelOptions);
